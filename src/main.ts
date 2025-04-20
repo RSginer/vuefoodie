562ebbda810d336d47919df1cb8cd2a72a2f73f6
Rubén Soler <r.solerginer@gmail.com>
2025-04-20 11:56:44 +0200

    Feature vue router

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,17 @@ import '@fontsource-variable/plus-jakarta-sans';
 import { createPinia } from 'pinia';
 import { createApp } from 'vue';
 import App from './App.vue';
+import HomePage from './pages/HomePage.vue';
+import { createRouter } from './router';
 
 const app = createApp(App)
 
-app.use(createPinia())
+app.use(createPinia()).use(createRouter({
+    routes: [
+        {
+            path: "/",
+            component: HomePage
+        }]
+}))
 
 app.mount('#app')
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="dark:bg-base-200 w-full rounded-2xl bg-white p-6 shadow-md">
+        <div class="card-body items-center text-center">
+            <h3 class="card-title mb-4 block text-2xl md:flex">Welcome to <div class="block xl:inline-block">
+                    <picture class="mx-2 block h-10 w-44 sm:h-12 sm:w-68 lg:mx-6">
+                        <source srcset="https://static.openfoodfacts.org/images/logos/off-logo-horizontal-dark.svg"
+                            media="(prefers-color-scheme: dark)"> <img
+                            src="https://static.openfoodfacts.org/images/logos/off-logo-horizontal-light.svg"
+                            alt="OpenFoodFacts Explorer" class="h-full w-full">
+                    </picture><!---->
+                </div> Explorer!</h3>
+            <p><!----><strong>OpenFoodFacts Explorer</strong> is a tool to explore the OpenFoodFacts database.<!---->
+            </p>
+            <p>You can enter a product code in the search bar above to get started or click on one of the products
+                below.
+            </p>
+        </div><!---->
+    </div>
+</template>
