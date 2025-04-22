459b13479d4d7bc0ff7d675c71c8cd28d4209c3c
Rubén Soler <r.solerginer@gmail.com>
2025-04-22 17:48:24 +0200

    Feature add skeleton loader

--- src/components/FancyQuestionList.vue
+++ src/components/FancyQuestionList.vue
@@ -1,10 +1,12 @@
 <template>
   <ul class="mt-4 flex flex-col gap-2">
-    <li v-if="!items.length">
-      Loading...
-    </li>
+    <template v-if="!items.length">
+      <li v-for="(_, index) in Array.from({ length: count })" :key="index">
+        <slot name="skeleton" />
+      </li>
+    </template>
     <li v-for="item in items" v-bind:key="item.barcode" class="flex flex-row gap-2">
-      <slot name="item" v-bind="item" />
+      <slot name="item" v-bind="item"></slot>
     </li>
   </ul>
 </template>
@@ -18,13 +20,17 @@ type QuestionResponse = {
   questions: Question[];
 }
 
-const { apiUrl } = defineProps(['apiUrl']);
+const { apiUrl, count } = defineProps(['apiUrl', 'count']);
 
 
-watch(() => apiUrl, async (newApiUrl) => {
+watch([() => apiUrl, () => count], async ([newApiUrl, newCount]) => {
   if (newApiUrl) {
     new Promise<QuestionResponse>((resolve) => {
       const url = new URL(newApiUrl);
+
+      if (newCount) {
+        url.searchParams.append('count', newCount.toString());
+      }
       
       fetch(url.toString())
         .then((response) => {
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,12 +1,18 @@
 <template>
     <WelcomeMessageCard />
-    <FancyQuestionList :apiUrl="apiUrl">
+    <FancyQuestionList :apiUrl="apiUrl" :count="5">
         <template #item="{ barcode, source_image_url }">
             <div class="flex flex-row gap-2 dark:bg-base-200 w-full rounded-2xl bg-white p-6 shadow-md">
-                <img class="w-20" :src="source_image_url" :alt="barcode + ' image'"/>
+                <img class="w-20 h-20" :src="source_image_url" :alt="barcode + ' image'"/>
                 <p>{{ barcode }}</p>
             </div>
-        </template>    
+        </template>
+        <template #skeleton>
+            <div class="flex flex-row gap-2 dark:bg-base-200 w-full rounded-2xl bg-white p-6 shadow-md">
+                <div class="skeleton w-20 h-20"></div>
+                <div class="skeleton w-full h-6"></div>
+            </div>
+        </template>
     </FancyQuestionList>
 </template>
 <script setup lang="ts">
@@ -14,5 +20,5 @@ import FancyQuestionList from "@/components/FancyQuestionList.vue";
 import WelcomeMessageCard from "@/components/WelcomeMessageCard.vue";
 import { ref } from "vue";
 
-const apiUrl = ref("https://robotoff.openfoodfacts.org/api/v1/questions?count=5");
+const apiUrl = ref("https://robotoff.openfoodfacts.org/api/v1/questions");
 </script>
