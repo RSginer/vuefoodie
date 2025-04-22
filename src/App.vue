9676a49d4ec5d8e297eb045dc4f8046b9adde41f
Rubén Soler <r.solerginer@gmail.com>
2025-04-22 12:25:55 +0200

    Feature add reactivity to the form

--- src/App.vue
+++ src/App.vue
@@ -19,8 +19,18 @@
 
 
 <script setup lang="ts">
+import { provide, ref, watch } from 'vue';
 import MainLayout from './components/MainLayout.vue';
 import PageCredits from './components/PageCredits.vue';
 import PageFooter from './components/PageFooter.vue';
 import PageHeader from './components/PageHeader.vue';
+import SearchQueryKey from './types/SearchQueryKey';
+
+const searchQuery = ref('');
+
+provide(SearchQueryKey, searchQuery);
+
+watch(searchQuery, () => {
+  console.log('Search query changed:', searchQuery.value);
+})
 </script>
--- src/components/QueryOrBarcodeForm.vue
+++ src/components/QueryOrBarcodeForm.vue
@@ -1,5 +1,16 @@
 <template>
-  <input type="text" class="input join-item input-bordered xl:w-full" placeholder="Query or barcode">
+  <input type="text" v-model="searchQuery" class="input join-item input-bordered xl:w-full" placeholder="Query or barcode">
   <button
+    :disabled="searchQuery.length === 0"
     class="btn btn-square btn-secondary join-item px-10">Go</button>
 </template>
+<script lang="ts" setup>
+import SearchQueryKey from '@/types/SearchQueryKey';
+import { inject, type Ref } from 'vue';
+
+const searchQuery = inject<Ref<string>>(SearchQueryKey);
+
+if (!searchQuery) {
+  throw new Error('searchQuery is not provided');
+}
+</script>
