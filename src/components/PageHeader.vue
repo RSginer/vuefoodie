fc34fc03695997375ece95c3c2a85eea308fe6e1
Rubén Soler <r.solerginer@gmail.com>
2025-04-22 12:15:29 +0200

    Add query or barcode form

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -1,6 +1,26 @@
 <template>
     <div class="navbar bg-base-100">
-        <img alt="OpenFoodFacts logo" class="logo" src="../assets/openfoodfacts-horizontal-logo.svg" width="272"
-            height="48" />
+        <div class="navbar-start">
+            <a class="btn-ghost btn text-xl normal-case" href="/">
+                <img alt="OpenFoodFacts logo" class="logo" src="../assets/openfoodfacts-horizontal-logo.svg" width="272"
+                    height="48" />
+            </a>
+        </div>
+        <div class="navbar-center">
+            <div class="form-control">
+                <div>
+                    <div class="join">
+                        <QueryOrBarcodeForm />
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="navbar-end gap-2">
+            <a class="btn btn-outline link" href="https://github.com/rsginer/vuefoodie" target="_blank" aria-label="GitHub"><span class="icon-[mdi--github] h-8 w-8"></span></a>
+        </div>
     </div>
 </template>
+<script setup lang="ts">
+import QueryOrBarcodeForm from './QueryOrBarcodeForm.vue';
+
+</script>
---
+++ src/components/QueryOrBarcodeForm.vue
@@ -0,0 +1,5 @@
+<template>
+  <input type="text" class="input join-item input-bordered xl:w-full" placeholder="Query or barcode">
+  <button
+    class="btn btn-square btn-secondary join-item px-10">Go</button>
+</template>
